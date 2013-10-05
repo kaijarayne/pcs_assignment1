adfa326b57ab0ff314f48ec8f0d492027ba14493
kaijarayne <kaijarayne@gmail.com>
2013-10-05 12:08:10 -0700

    pcs assignment 1

---
+++ variables.js
@@ -0,0 +1,10 @@
+var a = 1;
+var b = 3;
+var c = a*b
+var firstname = "ninja";
+var lastname = "turtle";
+var fullname = firstname + lastname;
+var g = fullname + c;
+console.log(c)
+console.log(fullname)
+console.log(g)
