42dcf1b663f8841287af6843511aaf5a57d21045
kaijarayne <kaijarayne@gmail.com>
2013-10-06 00:45:29 -0700

    assignment second try

---
+++ variables2.js
@@ -0,0 +1,12 @@
+var a = 5;
+var b = 7;
+var d = 9;
+var e = a*b-d;
+var firstSection = "You are a child";
+var secondSection = "Of the universe";
+var fullPhrase = firstSection + secondSection;
+var f = fullPhrase + e;
+console.log(e)
+console.log(fullPhrase)
+console.log(f)
+
