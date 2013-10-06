4da7586ea390c9bac1c38b533d8d4ef0c001be3c
kaijarayne <kaijarayne@gmail.com>
2013-10-05 18:29:00 -0700

    updates to assignment

--- variables.js
+++ variables.js
@@ -1,10 +1,10 @@
 var a = 1;
 var b = 3;
-var c = a*b
-var firstname = "ninja";
-var lastname = "turtle";
-var fullname = firstname + lastname;
-var g = fullname + c;
+var c = a*b;
+var firstName = "ninja";
+var lastName = "turtle";
+var fullName = firstName + lastName;
+var g = fullName + c;
 console.log(c)
-console.log(fullname)
+console.log(fullName)
 console.log(g)
