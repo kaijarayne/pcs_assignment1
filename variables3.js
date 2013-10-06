c3751b5bf5d1e8c8048259d5b7ef8347995880b2
kaijarayne <kaijarayne@gmail.com>
2013-10-06 00:59:13 -0700

    third try

---
+++ variables3.js
@@ -0,0 +1,14 @@
+var h = 5;
+var i = 7;
+var j = 9;
+var k = 11;
+var m = 13;
+var n = h + i * j - k * m;
+var firstPhrase = "this is"; 
+var secondPhrase = "pure ridiculousness";
+var thirdPhrase = firstPhrase + secondPhrase; 
+var o = thirdPhrase + n; 
+console.log(n);
+console.log(thirdPhrase);
+console.log(o);
+
